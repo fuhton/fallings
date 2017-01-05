1dbefb52ed70cb59b2303ccc22cdeac9f43aaec3
Nicholas Smith <nicholasparkersmith@gmail.com>
2017-01-04 22:38:32 -0500

    first pass at cleaning up repository without modifying plugin code

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,86 +1,72 @@
 module.exports = function(grunt) {
 
-    grunt.initConfig({
+	grunt.initConfig({
 
-        // Import package manifest
-        pkg: grunt.file.readJSON("fallings.jquery.json"),
+		// Import package manifest
+		pkg: grunt.file.readJSON("package.json"),
 
-        // Banner definitions
-        meta: {
-            banner: "/*\n" +
-                " *  <%= pkg.title || pkg.name %> - v<%= pkg.version %>\n" +
-                " *  <%= pkg.description %>\n" +
-                " *  <%= pkg.homepage %>\n" +
-                " *\n" +
-                " *  Made by <%= pkg.author.name %>\n" +
-                " *  Under <%= pkg.licenses[0].type %> License\n" +
-                " */\n"
-        },
+		// Banner definitions
+		meta: {
+			banner: "/*\n" +
+				" *  <%= pkg.title || pkg.name %> - v<%= pkg.version %>\n" +
+				" *  <%= pkg.description %>\n" +
+				" *  <%= pkg.homepage %>\n" +
+				" *\n" +
+				" *  Made by <%= pkg.author %>\n" +
+				" *  Under <%= pkg.license %> License\n" +
+				" */\n"
+		},
+		// Lint definitions
+		jshint: {
+			dist: ["src/jquery.fallings.js"],
+			options: {
+				jshintrc: ".jshintrc"
+			}
+		},
+		// Concat definitions
+		concat: {
+			dist: {
+				src: ["src/jquery.fallings.js"],
+				dest: "dist/jquery.fallings.js"
+			},
+			options: {
+				banner: "<%= meta.banner %>"
+			}
+		},
 
-        // Concat definitions
-        concat: {
-            dist: {
-                src: ["src/jquery.fallings.js"],
-                dest: "dist/jquery.fallings.js"
-            },
-            options: {
-                banner: "<%= meta.banner %>"
-            }
-        },
+		// Minify definitions
+		uglify: {
+			dist: {
+				src: ["dist/jquery.fallings.js"],
+				dest: "dist/jquery.fallings.min.js"
+			},
+			options: {
+				banner: "<%= meta.banner %>"
+			}
+		},
 
-        // Lint definitions
-        jshint: {
-            files: ["src/jquery.fallings.js"],
-            options: {
-                jshintrc: ".jshintrc"
-            }
-        },
+		less: {
+			development: {
+				options: {
+					paths: ["demo/styles"],
+					yuicompress: true
+				},
+				files: {
+					"demo/styles/style.css": "demo/styles/style.less"
+				}
+			}
+		},
+		watch: {
+			files: "demo/styles/*.less",
+			tasks: ["less"]
+		}
+	});
 
-        // Minify definitions
-        uglify: {
-            my_target: {
-                src: ["dist/jquery.fallings.js"],
-                dest: "dist/jquery.fallings.min.js"
-            },
-            options: {
-                banner: "<%= meta.banner %>"
-            }
-        },
+	grunt.loadNpmTasks("grunt-contrib-concat");
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-contrib-jshint");
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
-        // CoffeeScript compilation
-        coffee: {
-            compile: {
-                files: {
-                    "dist/jquery.fallings.js": "src/jquery.fallings.coffee"
-                }
-            }
-        },
-
-        less: {
-            development: {
-                options: {
-                    paths: ["demo/styles"],
-                    yuicompress: true
-                },
-                files: {
-                    "demo/styles/style.css": "demo/styles/style.less"
-                }
-            }
-        },
-        watch: {
-            files: "demo/styles/*.less",
-            tasks: ["less"]
-        }
-    });
-
-    grunt.loadNpmTasks("grunt-contrib-concat");
-    grunt.loadNpmTasks("grunt-contrib-jshint");
-    grunt.loadNpmTasks("grunt-contrib-uglify");
-    grunt.loadNpmTasks("grunt-contrib-coffee");
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-
-    grunt.registerTask("default", ["jshint", "concat", "uglify"]);
-    grunt.registerTask("travis", ["jshint"]);
-
+	grunt.registerTask("default", ["jshint", "concat", "uglify"]);
 };
